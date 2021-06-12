638ff496f0da266e053f278f334223c75c2cda61
Kirill Kotov <krlktv@mail.ru>
2021-06-12 21:49:32 +0300

    add activeSlide

--- app.js
+++ app.js
@@ -2,19 +2,25 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  const slides = document.querySelectorAll('.slide');
+  function slidesPlugin(activSlide = 0) {
+    const slides = document.querySelectorAll('.slide');
 
-  for (const slide of slides) {
-    slide.addEventListener('click', () => {
-      clearActiveClasses();
-      slide.classList.add('active');
-    });
+    slides[activSlide].classList.add('active');
+
+    for (const slide of slides) {
+      slide.addEventListener('click', () => {
+        clearActiveClasses();
+        slide.classList.add('active');
+      });
+    }
+
+    function clearActiveClasses() {
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+      });
+    }
   }
 
-  function clearActiveClasses() {
-    slides.forEach((slide) => {
-      slide.classList.remove('active');
-    });
-  }
+  slidesPlugin(3);
 
 });
